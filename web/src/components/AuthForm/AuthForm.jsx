313c30a5491ba56b2f1cecf8194c5faa6a883773
Akssses <itshnik07@gmail.com>
2025-07-22 20:12:19 +0600

    auth form

--- web/src/components/AuthForm/AuthForm.jsx
+++ web/src/components/AuthForm/AuthForm.jsx
@@ -4,7 +4,8 @@ import { useState } from "react";
 import s from "./AuthForm.module.scss";
 import { FaUser } from "react-icons/fa6";
 import { FaPhoneAlt } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
+import { HiUsers } from "react-icons/hi2";
+import { IoIosArrowDown } from "react-icons/io";
 
 export default function AuthForm() {
   const [name, setName] = useState("");
@@ -12,8 +13,19 @@ export default function AuthForm() {
   const [email, setEmail] = useState("");
   const [direction, setDirection] = useState("");
   const [agree, setAgree] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const directions = ["Математика ПО", "Геометрия", "Алгебра"];
+  const directions = [
+    "Математика + Физика",
+    "Математика + Информатика",
+    "Математика + География",
+    "Химия + Биология",
+    "География + Биология",
+    "Химия + Физика",
+    "Всемирная история + Человек. Общество. Право",
+    "Всемирная история + Английский язык",
+    "Всемирная история + География",
+  ];
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -33,62 +45,90 @@ export default function AuthForm() {
       </div>
 
       <form className={s.right} onSubmit={handleSubmit}>
-        <h2>Расскажите нам о себе</h2>
+        <div className={s.inpuuuts}>
+          <h2>Расскажите нам о себе</h2>
 
-        <div className={s.inputWrapper}>
-          <span>
-            <FaUser />
-          </span>
-          <input
-            type="text"
-            placeholder="Ваше имя"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+          <div className={s.inputWrapper}>
+            <span>
+              <FaUser />
+            </span>
+            <input
+              type="text"
+              placeholder="Ваше имя"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+
+          <div className={s.inputWrapper}>
+            <span>
+              <FaPhoneAlt />
+            </span>
+            <input
+              type="tel"
+              placeholder="Ваш телефон"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+
+          <div className={s.inputWrapper}>
+            <span>
+              <HiUsers />
+            </span>
+            <input
+              type="text"
+              placeholder="ФИО Родителя"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
         </div>
 
-        <div className={s.inputWrapper}>
-          <span>
-            <FaPhoneAlt />
-          </span>
-          <input
-            type="tel"
-            placeholder="Ваш телефон"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
-        </div>
+        <div className={s.napravlenie}>
+          <h2>Выберите направление:</h2>
+          <div className={s.dropdownWrapper}>
+            <div
+              className={s.dropdownHeader}
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+            >
+              {direction || "Направление"}
+              <span>
+                <IoIosArrowDown />
+              </span>
+            </div>
+            {isDropdownOpen && (
+              <ul className={s.dropdownList}>
+                {directions.map((item) => (
+                  <li
+                    key={item}
+                    onClick={() => {
+                      setDirection(item);
+                      setIsDropdownOpen(false);
+                    }}
+                  >
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
 
-        <div className={s.inputWrapper}>
-          <span>
-            <MdEmail />
-          </span>
-          <input
-            type="email"
-            placeholder="Ваш e-mail"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+          <label className={s.checkbox}>
+            <input
+              type="checkbox"
+              checked={agree}
+              onChange={(e) => setAgree(e.target.checked)}
+            />
+            <span>
+              Я согласен(а) с условиями хранения и обработки персональных данных
+            </span>
+          </label>
         </div>
-
-        <label className={s.checkbox}>
-          <input
-            type="checkbox"
-            checked={agree}
-            onChange={(e) => setAgree(e.target.checked)}
-          />
-          <span>
-            Я согласен(а) с условиями хранения и обработки персональных данных
-          </span>
-        </label>
 
         <button type="submit" className={s.button}>
           Приступить
         </button>
-
-        <a href="#" className={s.resultsLink}>
-          Посмотреть результаты
-        </a>
       </form>
     </div>
   );
