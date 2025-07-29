08632a4b071687b4cc8967e2772580a3ed060787
Your Name <you@example.com>
2025-07-29 14:28:25 +0600

    asd

--- server/apps/student_result/urls.py
+++ server/apps/student_result/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import StudentResultCreateView
+from .views import StudentResultCreateView, StudentResultDetailView
 
 urlpatterns = [
     path('create/', StudentResultCreateView.as_view(), name='student-result-create'),
+    path('result/<int:pk>/', StudentResultDetailView.as_view(), name='student-result-detail'),
+
 ]
--- server/apps/student_result/views.py
+++ server/apps/student_result/views.py
@@ -2,6 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import StudentResultSerializer
+from .models import StudentResult
+from django.shortcuts import get_object_or_404
+
 
 class StudentResultCreateView(APIView):
     def post(self, request):
@@ -10,3 +13,10 @@ class StudentResultCreateView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class StudentResultDetailView(APIView):
+    def get(self, request, pk):
+        result = get_object_or_404(StudentResult, pk=pk)
+        serializer = StudentResultSerializer(result)
+        return Response(serializer.data, status=status.HTTP_200_OK)
