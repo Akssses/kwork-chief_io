3a7400c0f4a9f615e447d875694f14e71fb32825
Your Name <you@example.com>
2025-09-05 15:15:19 +0600

    asd

--- server/ent/settings.py
+++ server/ent/settings.py
@@ -75,7 +75,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:9000",
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "https://kwork-chief-io.vercel.app"
+    "https://kwork-chief-io.vercel.app",
+    "https://jiyrma-gt.kz"
 ]
 CORS_ALLOW_METHODS = (
     "DELETE",
